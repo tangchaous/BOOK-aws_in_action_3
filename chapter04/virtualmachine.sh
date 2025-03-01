86643c1147dc61637c6a1d7a729ed78c3088b827
Huang Tang <htang@mitre.org>
2025-03-01 18:29:03 -0500

    auto-commit

--- chapter04/virtualmachine.sh
+++ chapter04/virtualmachine.sh
@@ -4,13 +4,16 @@ AMIID="$(aws ec2 describe-images --filters "Name=name,Values=amzn2-ami-hvm-2.0.2
 VPCID="$(aws ec2 describe-vpcs --filter "Name=isDefault, Values=true" --query "Vpcs[0].VpcId" --output text)"
 SUBNETID="$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPCID" --query "Subnets[0].SubnetId" --output text)"
 INSTANCEID="$(aws ec2 run-instances --image-id "$AMIID" --instance-type t3.micro --subnet-id "$SUBNETID" --iam-instance-profile "Name=ec2-ssm-core" --query "Instances[0].InstanceId" --output text)"
+
 echo "waiting for $INSTANCEID ..."
 aws ec2 wait instance-running --instance-ids "$INSTANCEID"
 echo "$INSTANCEID is up and running"
+
 echo "connect to the instance using Session Manager"
 echo "https://console.aws.amazon.com/systems-manager/session-manager/$INSTANCEID"
 read -r -p "Press [Enter] key to terminate $INSTANCEID ..."
 aws ec2 terminate-instances --instance-ids "$INSTANCEID" > /dev/null
+
 echo "terminating $INSTANCEID ..."
 aws ec2 wait instance-terminated --instance-ids "$INSTANCEID"
 echo "done."
